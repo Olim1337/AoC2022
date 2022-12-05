12f89564ed4ae2d40477e05f6699956a32a9886b
Oliver Moran <olim1337@gmail.com>
2022-12-05 23:29:09 +0000

    ez refactor

--- 5/5.py
+++ 5/5.py
@@ -1,7 +1,7 @@
 from collections import defaultdict, deque
+from copy import deepcopy
 stacks = defaultdict(deque)
 
-# Part 1
 with open("5/5.txt") as f:
     puzzle = f.read().split("\n")
     # parsing stack of crates
@@ -10,32 +10,18 @@ with open("5/5.txt") as f:
         for i, j in enumerate(row):
                 stacks[i+1].appendleft(j) if j != ' ' else None
 
-    # instructions
-    for row in puzzle[10:]:
-        n, f, t = [int(row.split(' ')[i]) for i in range(1,6,2)]
-        for _ in range(n):
-            stacks[t].append(stacks[f].pop())
+    stacks1, stacks2 = stacks, deepcopy(stacks)
 
-    part1 = ''.join([v[-1] for k,v in sorted(stacks.items())])
-    print(f"Part 1: {part1}")
-
-# Part 2
-stacks = defaultdict(deque)
-with open("5/5.txt") as f:
-    puzzle = f.read().split("\n")
-    # parsing stack of crates
-    for i in puzzle[:8]:
-        row = [i[j] for j in range(1, len(i), 4)]
-        for i, j in enumerate(row):
-                stacks[i+1].appendleft(j) if j != ' ' else None
-
-    # instructions
     for row in puzzle[10:]:
         n, f, t = [int(row.split(' ')[i]) for i in range(1,6,2)]
         temp = deque()
         for _ in range(n):
-            temp.append(stacks[f].pop())
-        [stacks[t].append(i) for i in reversed(temp)]
+            stacks1[t].append(stacks1[f].pop())
+            temp.append(stacks2[f].pop())
+        [stacks2[t].append(i) for i in reversed(temp)]
 
-    part2 = ''.join([v[-1] for k,v in sorted(stacks.items())])
+    part1 = ''.join([v[-1] for k,v in sorted(stacks1.items())])
+    print(f"Part 1: {part1}")        
+
+    part2 = ''.join([v[-1] for k,v in sorted(stacks2.items())])
     print(f"Part 2: {part2}")
