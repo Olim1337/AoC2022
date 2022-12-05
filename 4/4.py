3d0ad26dd87fd281ff4c0d9f11c0477e145db684
Oliver Moran <oliver.moran@enable.com>
2022-12-05 17:08:44 +0000

    tidy

--- 4/4.py
+++ 4/4.py
@@ -1,23 +1,11 @@
-#part 1
 with open ("4/4.txt") as f:
-    total = 0
-    rows = f.read().split("\n")[:-1]
-    for row in rows:
-        range1 = row.split(',')[0].split('-')
-        range2 = row.split(',')[1].split('-')
-        range1, range2 = [*range(int(range1[0]), int(range1[1])+1)], [*range(int(range2[0]), int(range2[1])+1)]
-        if range1 and range2 and (all(i in range1 for i in range2) or all(i in range2 for i in range1)):
-            total +=1
-    print(total)
+    part1 = part2 = 0
+    for row in f.read().split("\n")[:-1]:
+        range1, range2 = [i.split('-') for i in row.split(',')] 
+        range1, range2 = range(int(range1[0]), int(range1[1])+1), range(int(range2[0]), int(range2[1])+1)
+        if (all(i in range1 for i in range2) or all(i in range2 for i in range1)):
+            part1 +=1
+        if (any(i in range1 for i in range2)):
+            part2 +=1
 
-#part 2
-with open ("4/4.txt") as f:
-    total = 0
-    rows = f.read().split("\n")[:-1]
-    for row in rows:
-        range1 = row.split(',')[0].split('-')
-        range2 = row.split(',')[1].split('-')
-        range1, range2 = [*range(int(range1[0]), int(range1[1])+1)], [*range(int(range2[0]), int(range2[1])+1)]
-        if range1 and range2 and (any(i in range1 for i in range2)):
-            total +=1
-    print(total)
+print(f'Part 1: {part1}, Part 2: {part2}')
